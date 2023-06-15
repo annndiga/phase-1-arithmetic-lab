4b9cabd3ba4398f2bd244f39c1215cea8da49f5b
Ann Ndiga <annndiga@gmail.com>
2023-06-15 15:37:21 +0300

    Initialize git

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = 2 * 31;
+
+console.log(multiply);
+
+const min = 1; 
+const max = 20; 
+const random = Math.floor(Math.random() * (max - min + 1)) + min;
+
+console.log(random); 
+
+const num4 = 17;
+const num3 = 13;
+const mod = 17 % 13; 
+
+console.log(mod); 
+
